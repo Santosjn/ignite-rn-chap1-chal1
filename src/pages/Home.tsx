6a197608bf4cb1801756ba38334034a96665a081
Jean Novaes Santos <jeansjcsp@gmail.com>
2021-11-25 16:07:17 -0300

    solving the challenge.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,44 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    //TODO - add new task`
+    const task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    } as Task;
+
+    setTasks((oldState) => [...oldState, task]);
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const taskFound = tasks.find((task) => task.id === id);
+
+    if (taskFound) {
+      taskFound.done = !taskFound.done;
+      const tasksCopy = tasks.slice();
+
+      const index = tasks.findIndex((task) => task.id === id);
+      tasksCopy[index] = taskFound;
+      setTasks(tasksCopy);
+    }
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+
+    setTasks((oldState) => {
+      return oldState.filter((task) => task.id !== id);
+    });
   }
 
   return (
@@ -26,18 +47,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
